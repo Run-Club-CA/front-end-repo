e98841d0e5ec2d680137cb18507174659f9b8ca4
Liam-M-Dev <liammassey21@gmail.com>
2023-07-22 15:46:32 +1000

    scaffold basic starting layout for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+
+export default function LandingPage(){
+    return(
+        <div>
+            <h1>Login form or sign up form to be rendered here</h1>
+        </div>
+    );
+    
+}
