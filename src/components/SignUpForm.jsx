b29403f9ce12dbafeb753893f7c02a989410ab98
Liam-M-Dev <liammassey21@gmail.com>
2023-07-26 13:29:03 +1000

    Scaffold basic sign up form

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,30 @@
+
+export default function SignUpForm(){
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+
+    return(
+        <div>
+            <form name="sign up form">
+                <label>First Name</label>
+                <input type="text" />
+
+                <label>Last Name</label>
+                <input type="text" />
+
+                <label>Email</label>
+                <input type="email" />
+
+                <label>UserName</label>
+                <input type="text" />
+
+                <label>Password</label>
+                <input type="password" /> 
+            </form>
+        </div>
+        
+    )
+}
