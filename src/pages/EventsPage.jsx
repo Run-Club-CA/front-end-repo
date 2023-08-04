58bd919f517d2a9765cf6ca65ae3344818334dec
Laurence-W <laurenceother@hotmail.com>
2023-08-04 23:35:21 +1000

    only trainers and admin will see create event component

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,10 +1,22 @@
 import CreateEvent from "../components/CreateEvent";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function EventsPage() {
-  return (
-    <div>
-      <h1>Events </h1>
-      <CreateEvent />
-    </div>
-  );
+  const { isTrainer } = useAuth();
+  const { isAdmin } = useAuth();
+
+  if (isTrainer === true || isAdmin === true) {
+    return (
+      <div>
+        <h1>Events </h1>
+        <CreateEvent />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Events </h1>
+      </div>
+    );
+  }
 }
