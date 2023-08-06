af0928fcc3aeb9e676490222431130d3984175cc
Laurence-W <laurenceother@hotmail.com>
2023-08-06 11:27:27 +1000

    added total km run feature

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -2,36 +2,57 @@ import { useEffect } from "react";
 import { useUser } from "../contexts/UserContext";
 import { useAuth } from "../contexts/AuthContext";
 import { getUser } from "../services/UserServices";
+import React, { useState } from "react";
+import { getUserCompletedEvents } from "../services/EventServices";
 
 // Renders with the user's name, logout and edit profile links
 export default function DashBoardComponent(props) {
+  const { userDetails, storeUserDetails, removeUserDetails } = useUser();
+  const { user, logout, isTrainer, isAdmin } = useAuth();
+  const [userCompletedRuns, setuserCompletedRuns] = useState([]);
 
-    const {userDetails, storeUserDetails, removeUserDetails} = useUser();
-    const {user, logout, isTrainer, isAdmin} = useAuth();
+  useEffect(() => {
+    getUserCompletedEvents(user)
+      .then((data) => setuserCompletedRuns(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    // useEffect hook to get user details and store them in local storage
-    // Uses the supplied JWT that relates to the current user
-    useEffect(() => {
-        console.log(user)
-        getUser(user)
-        .then(data => storeUserDetails(data))
-        .catch(error => console.log(error));
-        console.log(userDetails);
-    }, []);
+  // const userCompletedRunsItems = userCompletedRuns.map((run) => (
+  //   <li key={run.id}>
+  //     <p>{run.name}</p>
+  //     <span>{run.distance}</span>
+  //   </li>
+  // ));
 
-    const logoutUser = () => {
-        logout();
-        removeUserDetails();
-    }
+  var totalRun = userCompletedRuns.reduce(
+    (accum, item) => accum + item.distance,
+    0
+  );
 
-    return(
-        <div className="text-white bg-grey-div mx-auto my-10 w-mobile-width h-mobile-height p-4 rounded-main-div shadow-mobile-shadow">
-            <h1>Welcome! {userDetails.firstName || "Testname"}</h1>
-            <a href="/dashboard/profile">Edit Profile</a>
-            <a href="/dashboard">Home</a>
-            {(isTrainer || isAdmin) && <a href="/dashboard/events">Create Event</a>}
-            {isAdmin && <a href="/dashboard/admin/users">Users List</a>}
-            <button onClick={logoutUser}>Logout</button>
-        </div>
-    )
+  // useEffect hook to get user details and store them in local storage
+  // Uses the supplied JWT that relates to the current user
+  useEffect(() => {
+    console.log(user);
+    getUser(user)
+      .then((data) => storeUserDetails(data))
+      .catch((error) => console.log(error));
+    console.log(userDetails);
+  }, []);
+
+  const logoutUser = () => {
+    logout();
+    removeUserDetails();
+  };
+
+  return (
+    <div className="text-white bg-grey-div mx-auto my-10 w-mobile-width h-mobile-height p-4 rounded-main-div shadow-mobile-shadow">
+      <h1>Welcome! {userDetails.firstName || "Testname"}</h1>
+      <p>Total run:{totalRun}km </p>
+      <a href="/dashboard/profile">Edit Profile</a>
+      <a href="/dashboard">Home</a>
+      {(isTrainer || isAdmin) && <a href="/dashboard/events">Create Event</a>}
+      {isAdmin && <a href="/dashboard/admin/users">Users List</a>}
+      <button onClick={logoutUser}>Logout</button>
+    </div>
+  );
 }
